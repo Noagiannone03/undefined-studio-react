fb23e86dfdd08039ac486145579a3e6e39e47dfd
Noa Giannone <noa.giannone@gmail.com>
2026-01-29 14:29:54 +0100

    feat: Implement neo-brutalist styling and animations for the menu button, adjusting its position and hover effects.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -30,26 +30,34 @@ export default function Menu() {
 
     return (
         <div ref={container}>
-            <div className="fixed top-20 right-8 z-[60]">
+            <div className="fixed top-8 right-8 z-[60]">
+                {/* NEO-BRUTALIST BUTTON */}
                 <button
                     onClick={toggleMenu}
-                    className="group relative w-16 h-16 transition-transform active:scale-90"
+                    className="group relative w-16 h-16 outline-none focus:outline-none"
                 >
-                    {/* Shadow Layer */}
-                    <div className="absolute inset-0 bg-black translate-x-1 translate-y-1 rounded-full group-hover:translate-x-0.5 group-hover:translate-y-0.5 transition-transform" />
-                    {/* Main Layer */}
-                    <div className={`relative w-full h-full ${isOpen ? 'bg-black text-white' : 'bg-lemon text-black'} border-4 border-black rounded-full flex items-center justify-center transition-colors duration-300 shadow-hard group-hover:-translate-x-0.5 group-hover:-translate-y-0.5`}>
+                    {/* Shadow Block (Bottom Layer) - FIXED */}
+                    <div className="absolute inset-0 bg-black rounded-full translate-x-1 translate-y-1" />
+
+                    {/* Main Button (Top Layer) - MOVES ON CLICK */}
+                    <div className={`
+                        relative w-full h-full rounded-full border-4 border-black flex items-center justify-center transition-all duration-100 ease-out
+                        ${isOpen ? 'bg-black text-white' : 'bg-mint text-black'}
+                        translate-x-0 translate-y-0
+                        group-hover:-translate-y-0.5 group-hover:-translate-x-0.5
+                        group-active:translate-x-1 group-active:translate-y-1
+                    `}>
                         <div className="flex flex-col gap-1.5 items-center justify-center w-8">
                             {isOpen ? (
-                                <div className="relative w-full h-6">
-                                    <div className="absolute top-1/2 left-0 w-full h-1 bg-white rotate-45" />
-                                    <div className="absolute top-1/2 left-0 w-full h-1 bg-white -rotate-45" />
+                                <div className="relative w-full h-6 pointer-events-none">
+                                    <div className="absolute top-1/2 left-0 w-full h-1 bg-white rotate-45 transition-transform" />
+                                    <div className="absolute top-1/2 left-0 w-full h-1 bg-white -rotate-45 transition-transform" />
                                 </div>
                             ) : (
                                 <>
-                                    <div className="w-full h-1 bg-black rounded-full" />
-                                    <div className="w-2/3 h-1 bg-black rounded-full self-start" />
-                                    <div className="w-full h-1 bg-black rounded-full" />
+                                    <div className="w-full h-1 bg-black rounded-full transition-all group-hover:w-3/4" />
+                                    <div className="w-2/3 h-1 bg-black rounded-full self-start transition-all group-hover:w-full" />
+                                    <div className="w-full h-1 bg-black rounded-full transition-all group-hover:w-1/2" />
                                 </>
                             )}
                         </div>
